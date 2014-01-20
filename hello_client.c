8de2bec6db391c4739be2274b2ccf9dda5fb6a21
J.B. Lee <jblee3@gmail.com>
2014-01-20 16:53:20 +0900

    study 20th Jan. 2014

--- hello_client.c
+++ hello_client.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]){
 
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
     serv_addr.sin_port = htons(atoi(argv[2]));
 
     if(connect(sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
--- hello_server.c
+++ hello_server.c
@@ -24,6 +24,7 @@ int main(int argc, char *argv[]){
     serv_sock = socket(AF_INET, SOCK_STREAM, 0);
     if(serv_sock == -1)
         error_handling("socket() error");
+    printf("Done : socket \n");
 
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
@@ -32,18 +33,23 @@ int main(int argc, char *argv[]){
 
     if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
         error_handling("bind() error");
+    printf("Done : bind \n");
 
     if(listen(serv_sock, 5) == -1)
         error_handling("listen() error");
+    printf("Done : listen \n");
 
     clnt_addr_size = sizeof(clnt_addr);
     clnt_sock = accept(serv_sock, (struct sockaddr*) &clnt_addr, &clnt_addr_size);
     if(clnt_sock == -1)
         error_handling("accept() error");
+    printf("Done : accept \n");
 
     write(clnt_sock, messege, sizeof(messege));
+    printf("Done : write \n");
     close(clnt_sock);
     close(serv_sock);
+    printf("Done : close \n");
     return 0;
 }
 
