752ebb1ced1d26336322389267aa6127763cce88
J.B. Lee <jblee3@gmail.com>
2014-02-11 12:50:01 +0900

    11th Feb. 2014

---
+++ sep_serv.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 1024
+void error_handling(char *message);
+
+int main(int argc, char *argv[]){
+    int serv_sock, clnt_sock;
+    FILE * readfp;
+    FILE * writefp;
+
+    struct sockaddr_in serv_addr, clnt_addr;
+    socklen_t clnt_addr_size;
+    char buf[BUF_SIZE] = {0,};
+
+    if(argc != 2){
+        printf("Usage : %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    serv_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(serv_sock == -1)
+        error_handling("socket() error");
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    if(bind(serv_sock, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1)
+        error_handling("bind() error");
+
+    if(listen(serv_sock, 5) == -1)
+        error_handling("listen() error");
+
+    clnt_addr_size = sizeof(clnt_addr);
+    clnt_sock = accept(serv_sock, (struct sockaddr*) &clnt_addr, &clnt_addr_size);
+    readfp = fdopen(clnt_sock, "r");
+    writefp = fdopen(clnt_sock, "w");
+
+    fputs("FROM SERVER : Hi ~ client? \n", writefp);
+    fputs("I love all of the world \n", writefp);
+    fputs("You are awesome ! \n", writefp);
+    fflush(writefp);
+
+    fclose(writefp);
+    fgets(buf, sizeof(buf), readfp);
+    fputs(buf, stdout);
+    fclose(readfp);
+    return 0;
+}
+
+void error_handling(char *message){
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
